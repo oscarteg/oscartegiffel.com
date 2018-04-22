f98ef7632233f59ff5e03a2bc0e8fa26ffabd0c2
Oscar te Giffel <oscar@tegiffel.com>
2018-04-22 20:22:44 +0200

    Added some things

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -1,7 +1,12 @@
 <template>
   <ul>
-      <router-link :to="{ name: 'AboutMe'}" tag="li"><a>over mij</a></router-link>
-      <router-link :to="{ name: 'WhatICan'}" tag="li"><a>wat ik kan</a></router-link>
+    <!-- TODO: Add icons for every route -->
+      <router-link :to="{ name: 'AboutMe'}" tag="li">
+        <a>over mij</a>
+        </router-link>
+      <router-link :to="{ name: 'WhatICan'}" tag="li">
+        <a><font-awesome-icon class="fa-fw" :icon="['fas', 'code']" />wat ik kan</a>
+        </router-link>
       <router-link :to="{ name: 'WhatIHaveLearned'}" tag="li"><a>wat ik heb geleerd</a></router-link>
       <router-link :to="{ name: 'WhatIHaveDone'}" tag="li"><a>wat ik heb gedaan</a></router-link>
       <router-link :to="{ name: 'Contact'}" tag="li"><a>contact</a></router-link>
@@ -9,7 +14,12 @@
 </template>
 
 <script>
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+
 export default {
-  name: "Navigation"
+  name: "Navigation",
+  components: {
+    FontAwesomeIcon
+  }
 };
 </script>
