35fb5aeff95b6e957c34abda29606b88fc179aeb
Oscar te Giffel <oscar@tegiffel.com>
2018-05-29 04:23:10 +0200

    Removed unnecessary dependencies

--- src/main.js
+++ src/main.js
@@ -8,19 +8,8 @@ import _ from "lodash";
 
 import App from "./App.vue";
 import router from "./router";
-import store from "./store";
-import "./registerServiceWorker";
 import "./assets/sass/main.scss";
 
-/*
-
-Add styling to Vue
-
-Buefy 
-Fontawesome
-Vue loading
-
-*/
 Vue.use(Buefy, {
   defaultIconPack: "fas"
 });
@@ -40,6 +29,5 @@ Vue.config.productionTip = false;
 
 new Vue({
   router,
-  store,
   render: h => h(App)
 }).$mount("#app");
