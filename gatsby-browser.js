256f59a8965b678269410119a4104669d049f007
Oscar te Giffel <oscar@tegiffel.com>
2019-01-27 11:56:51 +0100

    Reload on update service worker

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -5,3 +5,8 @@
  */
 
 // You can delete this file if you're not using it
+
+// When service werker is updated, reload the new information
+exports.onServiceWorkerUpdateFound = () => {
+    window.location.reload()
+}
