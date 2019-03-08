b1414bc87b9502a7fb0fd6c83b76ce921e668256
Oscar te Giffel <oscar@tegiffel.com>
2019-03-08 11:42:04 +0100

    Updated footer
    Signed-off-by: Oscar te Giffel <oscar@tegiffel.com>

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,6 @@
-import React from 'react'
-import FooterList from './FooterList'
-import { StaticQuery, graphql } from 'gatsby';
+import React from `react`
+import FooterList from `./FooterList`
+import { StaticQuery, graphql } from `gatsby`;
 
 const Footer = () => (
   <StaticQuery 
@@ -18,34 +18,35 @@ const Footer = () => (
       <div className="max-w-lg mx-auto pt-2">
         <div className="md:flex mb-4 text-center md:text-left justify-center">
           <FooterList
-            title={'Frontend'}
+            title={`Frontend`}
             items={[
-              'ES 5/6/7 en Typescript',
-              'React (Redux / SPA met React Router)',
-              'Vue (Vuex / SPA met Vue Router)',
-              'React Native',
-              'CSS (SCSS/SASS)',
-              'Bootstrap/Bulma/Tailwindcss',
-              'Android (beginner)',
+              `ES 5/6/7 en Typescript`,
+              `React (Redux / SPA met React Router)`,
+              `Vue (Vuex / SPA met Vue Router)`,
+              `React Native`,
+              `CSS (SCSS/SASS)`,
+              `Bootstrap/Bulma/Tailwindcss`,
+              `REST/Graphql`,
+              `Android (beginner)`,
             ]}
           />
 
           <FooterList
-            title={'Backend'}
-            items={['PHP (Laravel)', 'Java', 'GoLang (beginner)']}
+            title={`Backend`}
+            items={[`PHP (Laravel)`, `Java`, `GoLang (beginner)`]}
           />
 
           <FooterList
-            title={'Database/Devops'}
+            title={`Database/Devops`}
             items={[
-              'Docker',
-              'Gitlab CI/CD',
-              'Linux',
-              'MySQL',
-              'Postgresql',
-              'Nginx/Apache',
-              'Vagrant',
-              'Ansible',
+              `Docker`,
+              `Gitlab CI/CD`,
+              `Linux`,
+              `MySQL`,
+              `Postgresql`,
+              `Nginx/Apache`,
+              `Vagrant`,
+              `Ansible`,
             ]}
           />
         </div>
