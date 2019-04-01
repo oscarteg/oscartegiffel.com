8d5c75b00218014585721f32959d77949f67afef
Oscar te Giffel <oscar@tegiffel.com>
2019-04-01 21:51:23 +0200

    Added some fonts
    Signed-off-by: Oscar te Giffel <oscar@tegiffel.com>

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -10,21 +10,3 @@
 exports.onServiceWorkerUpdateFound = () => {
   window.location.reload()
 }
-
-exports.shouldUpdateScroll = ({
-  routerProps: { location },
-  getSavedScrollPosition,
-}) => {
-  const transitionDelay = 500
-
-  if (location.action === 'PUSH') {
-    window.setTimeout(() => window.scrollTo(0, 0), transitionDelay)
-  } else {
-    const savedPosition = getSavedScrollPosition(location)
-    window.setTimeout(
-      () => window.scrollTo(...(savedPosition || [0, 0])),
-      transitionDelay
-    )
-  }
-  return false
-}
--- src/assets/typography.js
+++ src/assets/typography.js
@@ -1,7 +1,15 @@
 import Typography from 'typography'
 import grandViewTheme from 'typography-theme-grand-view'
 
-const typography = new Typography(grandViewTheme)
+grandViewTheme.overrideThemeStyles = () => ({})
+
+const typography = new Typography({
+    ...grandViewTheme,
+    bodyFontFamily: ['Proxima Nova'],
+    headerFontFamily: ['Maison Neue'],
+  },
+)
+// const typography = new Typography(grandViewTheme)
 
 // Hot reload typography in development.
 if (process.env.NODE_ENV !== `production`) {
