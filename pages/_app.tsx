5bbb551d28683db0c5fdfe4710eb79d27e56a7fb
Oscar te Giffel <oscar@tegiffel.com>
2021-05-31 14:25:05 +0200

    Removed comma number, added security headers

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,10 +2,19 @@ import {MDXProvider} from '@mdx-js/react';
 import 'katex/dist/katex.min.css';
 import {DefaultSeo} from 'next-seo';
 import {ThemeProvider} from 'next-themes';
+import dynamic from 'next/dynamic';
 import Head from 'next/head';
+import 'nprogress/nprogress.css';
 import MDXComponents from '../components/MDXComponents';
 import SEO from '../next-seo.config';
 import '../styles/global.css';
+
+const ProgressBar = dynamic(
+  () => {
+    return import('../components/progress-bar');
+  },
+  {ssr: false}
+);
 
 export default function App({Component, pageProps}) {
   return (
@@ -15,6 +24,7 @@ export default function App({Component, pageProps}) {
           <meta content="width=device-width, initial-scale=1" name="viewport" />
         </Head>
         <DefaultSeo {...SEO} />
+        <ProgressBar />
         <Component {...pageProps} />
       </MDXProvider>
     </ThemeProvider>
