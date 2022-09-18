63512d8b97e342708a0f38d7f940ca948d623a3e
Oscar te Giffel <oscar@tegiffel.com>
2022-09-19 01:08:39 +0700

    feat: added divider as notion type to fix blog

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,10 +1,27 @@
 import React from "react";
-import { blocksToReactComponents } from "notion-blocks-react-renderer";
+import { blockToText, blocksToReactComponents, BLOCKS, blockRenderer } from "notion-blocks-react-renderer";
 
 type Props = {
   blocks: any;
 };
 
+const renderers = {
+  [BLOCKS.PARAGRAPH]: (block) => <p>{blockToText(block)}</p>,
+  [BLOCKS.HEADING1]: (block) => <h1>{blockToText(block)}</h1>,
+  [BLOCKS.HEADING2]: (block) => <h2>{blockToText(block)}</h2>,
+  [BLOCKS.HEADING3]: (block) => <h3>{blockToText(block)}</h3>,
+  [BLOCKS.BULLETEDLISTITEM]: (block) => <li>{blockToText(block)}</li>,
+  [BLOCKS.BULLETEDLIST]: (block) => <ul>{block.list.map((listItem) => blockRenderer(listItem, renderers))}</ul>,
+  [BLOCKS.NUMBEREDLISTITEM]: (block) => <li>{blockToText(block)}</li>,
+  [BLOCKS.NUMBEREDLIST]: (block) => <ol>{block.list.map((listItem) => blockRenderer(listItem, renderers))}</ol>,
+  [BLOCKS.QUOTE]: (block) => <blockquote>{blockToText(block)}</blockquote>,
+  [BLOCKS.CODE]: (block) => <code>{blockToText(block)}</code>,
+  [BLOCKS.IMAGE]: (block) => <img src={block.image.file.url} />,
+  [BLOCKS.AUDIO]: (block) => <audio controls preload="none" src={block.audio.file.url} />,
+  ["divider"]: (block) => <div className="w-full my-8 border border-gray-200 dark:border-gray-600" />,
+};
+
 export default function Content({ blocks }: Props) {
-  return <div>{blocksToReactComponents(blocks)}</div>;
+  console.log(blocks.results);
+  return <>{blocksToReactComponents(blocks.results, renderers)}</>;
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,25 +2,19 @@ const { spacing, fontFamily } = require("tailwindcss/defaultTheme");
 
 module.exports = {
   darkMode: "class",
-  content: ["./public/**/*.html", "./src/**/*.{astro,js,jsx,ts,tsx}"],
+  content: ["./public/**/*.html", "./src/**/*.{astro,ts,tsx}"],
   theme: {
     extend: {
       backgroundImage: {
         "gradient-conic": "conic-gradient(var(--tw-gradient-stops))",
         "gradient-conic-t": "conic-gradient(at top, var(--tw-gradient-stops))",
-        "gradient-conic-r":
-          "conic-gradient(at right, var(--tw-gradient-stops))",
-        "gradient-conic-b":
-          "conic-gradient(at bottom, var(--tw-gradient-stops))",
+        "gradient-conic-r": "conic-gradient(at right, var(--tw-gradient-stops))",
+        "gradient-conic-b": "conic-gradient(at bottom, var(--tw-gradient-stops))",
         "gradient-conic-l": "conic-gradient(at left, var(--tw-gradient-stops))",
-        "gradient-conic-tr":
-          "conic-gradient(at top right, var(--tw-gradient-stops))",
-        "gradient-conic-tl":
-          "conic-gradient(at top left, var(--tw-gradient-stops))",
-        "gradient-conic-br":
-          "conic-gradient(at bottom right, var(--tw-gradient-stops))",
-        "gradient-conic-bl":
-          "conic-gradient(at bottom left, var(--tw-gradient-stops))",
+        "gradient-conic-tr": "conic-gradient(at top right, var(--tw-gradient-stops))",
+        "gradient-conic-tl": "conic-gradient(at top left, var(--tw-gradient-stops))",
+        "gradient-conic-br": "conic-gradient(at bottom right, var(--tw-gradient-stops))",
+        "gradient-conic-bl": "conic-gradient(at bottom left, var(--tw-gradient-stops))",
       },
       fontFamily: {
         sans: ["IBM Plex Sans", ...fontFamily.sans],
@@ -96,9 +90,5 @@ module.exports = {
       }),
     },
   },
-  plugins: [
-    require("@tailwindcss/typography"),
-    require("@tailwindcss/forms"),
-    require("@tailwindcss/line-clamp"),
-  ],
+  plugins: [require("@tailwindcss/typography"), require("@tailwindcss/forms"), require("@tailwindcss/line-clamp")],
 };
