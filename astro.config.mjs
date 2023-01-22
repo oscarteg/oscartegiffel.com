4a8a56906824084ccbf4ad05a143bce8127df7aa
Oscar te Giffel <oscar@tegiffel.com>
2023-01-22 18:51:02 +0100

    build: edge deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 import image from "@astrojs/image";
-import vercel from "@astrojs/vercel/edge";
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
