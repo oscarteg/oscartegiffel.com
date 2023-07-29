c6e123a67196ec8566cef4047e18fcb7f749f489
Oscar te Giffel <oscar@tegiffel.com>
2023-07-30 01:24:17 +0200

    fix: removed lit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,7 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import tailwind from "@astrojs/tailwind";
-import vercel from "@astrojs/vercel/edge";
-import lit from "@astrojs/lit";
+import vercel from "@astrojs/vercel/serverless";
 import { SITE_URL } from "./src/config";
 import sitemap from "@astrojs/sitemap";
 
@@ -12,6 +11,9 @@ export default defineConfig({
   adapter: vercel({
     analytics: true,
   }),
+  build: {
+    split: true,
+  },
   site: SITE_URL,
   integrations: [
     mdx({
@@ -20,7 +22,6 @@ export default defineConfig({
     tailwind({
       applyBaseStyles: false,
     }),
-    lit(),
     sitemap({
       filter: (page) => !page.includes("/api/"),
     }),
