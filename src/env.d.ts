7d6638d642172d55fadb24550eab68cbd59ed6ff
Oscar te Giffel <oscar@tegiffel.com>
2024-02-11 21:12:07 +0100

    feat: fixed build

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,8 +1,8 @@
 /* eslint-disable @typescript-eslint/triple-slash-reference */
 /// <reference path="../.astro/types.d.ts" />
-import "../.astro/types.d.ts";
 
 interface InterfaceMetaEnv {
+  readonly PROD: boolean;
   readonly PUBLIC_DEPOY_HOOK: string;
 }
 
--- src/pages/api/build-and-deploy.ts
+++ src/pages/api/build-and-deploy.ts
@@ -1,9 +1,7 @@
 import type { APIRoute } from "astro";
 
-import type { ImportMeta } from "../../env";
-
 // HACK: This is a hack to get around the fact that the `env` variable is not available in the `astro` module.
-const { PUBLIC_DEPLOY_HOOK } = (import.meta as unknown as ImportMeta).env;
+const { PUBLIC_DEPLOY_HOOK } = import.meta.env;
 
 export const get: APIRoute = async function get() {
   return fetch(PUBLIC_DEPLOY_HOOK, {
