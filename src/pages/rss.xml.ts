d8d93e714602690fee3e9f2ae36dd85e51aa5511
Oscar te Giffel <oscar@tegiffel.com>
2024-09-01 14:10:46 +0200

    fix: rss feed sorted

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -1,14 +1,17 @@
 import rss from "@astrojs/rss";
 import { getCollection } from "astro:content";
 import { SITE_DESCRIPTION, SITE_TITLE, SITE_URL } from "../config";
+import { isPublished, sortAsc } from "../utils";
 
 export async function GET() {
-  const posts = await getCollection("blog");
+  const posts = await getCollection("blog", isPublished);
+  const sortedPosts = posts.sort(sortAsc);
+
   return rss({
     title: SITE_TITLE,
     description: SITE_DESCRIPTION,
     site: SITE_URL,
-    items: posts.map((post) => ({
+    items: sortedPosts.map((post) => ({
       title: post.data.title,
       pubDate: post.data.publishDate,
       description: post.data.summary,
