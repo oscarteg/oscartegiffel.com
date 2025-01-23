7236a10252094e43dfa67fb49f5f7cd407313c40
Oscar te Giffel <oscar@tegiffel.com>
2025-01-23 16:17:25 +0100

    feat: cleanup and add satisifes typescript post

--- astro.config.mjs
+++ astro.config.mjs
@@ -20,9 +20,6 @@ export default defineConfig({
     mdx({
       drafts: true,
     }),
-    tailwind({
-      applyBaseStyles: false,
-    }),
     sitemap(),
     solidJs(),
   ],
--- cypress/e2e/index.cy.ts
+++
@@ -1,6 +0,0 @@
-it("titles are correct", () => {
-  const page = cy.visit("http://localhost:3000");
-
-  page.get("title").should("contain", "Oscar te Giffel");
-  page.get("h1").should("contain.text", "Oscar te Giffel");
-});
